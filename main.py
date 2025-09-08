a09971be90fbeb65b2386cdb206b3696a8a01eef
nicoapallaz <nicholasflores2006@gmail.com>
2025-09-07 23:21:45 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+hour = int(input("Enter the KW hours used: "))
+print()
+if hour > 1000:
+    amount = 7.633 * 1000
+    hour -= 1000
+    amount = amount + (9.259 * hour)
+    amount /= 100
+    total = round(amount, 2)
+else:
+    amount = 7.633 * hour
+    amount /= 100
+    total = round(amount, 2)
+print("Amount owed is $" + str(total))
